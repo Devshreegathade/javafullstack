b938d870730f7e31b2506545106b757b17a9eccc
Devshreegathade <gdevshree01@gmail.com>
2026-02-21 23:31:14 +0530

    modified

--- src/basic/printArray.java
+++ src/basic/printArray.java
@@ -9,17 +9,16 @@ public class printArray {
         System.out.print("Enter how many numbers : ");
         int n = sc.nextInt();
 
-        int[] numbers = new int[n];
+        int[] numbers = new int[n];       //array creation
         int sum = 0;
 
         for (int i = 0;i < n; i++){
             System.out.print("enter the number : ");
-            numbers[i] = sc.nextInt();
+            numbers[i] = sc.nextInt();           //array input
 
         }
         for (int i = 0;i < n; i++){
             System.out.println(numbers[i]);
-
         }
         sc.close();
     }
