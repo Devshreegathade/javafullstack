e5e61ffc13996756c8f34098f4da11949c32afb2
Devshreegathade <gdevshree01@gmail.com>
2026-03-06 22:54:04 +0530

    initial commit

---
+++ src/basic/Factorial.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a non-negative integer: ");
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+            return;
+        }
+
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial of " + n + " is: " + fact);
+        sc.close();
+    }
+}
